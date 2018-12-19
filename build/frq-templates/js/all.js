cb304f06f59e509f4631fb26ec2922f03caba695
GunaySeyidahmed <gunay.seyidehmedqizi@gmail.com>
2018-12-18 23:18:58 -0800

    first commit

--- build/frq-templates/js/all.js
+++ build/frq-templates/js/all.js
@@ -1,3 +1,30 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+
+    $(window).scroll(function () {
+        if($(this).scrollTop()>70){
+            $('.menu__nav').addClass('menu__sticky');
+            $('.menu img:nth-child(1)').addClass('d-none');
+            $('.menu img:nth-child(2)').removeClass('d-none').addClass('d-inline-block');
+            $('.nav-link').removeClass('white-text').addClass('black-text');
+        }
+        else {
+            $('.menu__nav').removeClass('menu__sticky');
+            $('.menu img:nth-child(1)').removeClass('d-none');
+            $('.menu img:nth-child(2)').addClass('d-none').removeClass('d-inline-block');
+            $('.nav-link').addClass('white-text').removeClass('black-text');
+        }
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 250) {
+            $('.scroll-up').fadeIn("slow").css("display", "block");
+        } else {
+            $('.scroll-up').fadeOut("slow").css("display", "none");
+        }
+    });
+    $(".scroll-up").click(function (event) {
+        event.preventDefault();
+        $("html, body").animate({scrollTop: 0}, "slow");
+        return false;
+    });
 });
